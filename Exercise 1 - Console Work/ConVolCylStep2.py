af27f634a884f3a6257e22bac7b244827f2030c1
Zachary Gopinath <zachary.gopinath@GCCYJPRUJ1WK.local>
2019-09-17 12:01:35 -0400

    Exercise 1 step 2

--- Exercise 1 - Console Work/ConVolCylStep2.py
+++ Exercise 1 - Console Work/ConVolCylStep2.py
@@ -1,23 +1,33 @@
-import 
+import math
 #input
 #numbers needed to find volume of cylinder, height and radius/diameter
 
 print("Volume of a Cylinder Formula:")
 
-name = input("What is your name: ")   #takes users name
+print("\n\tThe volume of a Cylinder is:")
+print("\n\t\t\tV = \u03C0\u00d7radius\u00b2\u00d7height")
+print("\n\tThis program will take as input the radius and height")
+print("\tand print the volume.")
 
-radius = input("Input radius(cm): ")  #input
+name = input("\n\t\t\tWhat is your name: ")   #takes users name
+
+radius = input("\n\t\t\tInput radius(cm): ")  #input
 radius = (int)(radius)     #cast to int
 
-height = input("Input height(cm): ") #input
+height = input("\n\t\t\tInput height(cm): ") #input
 height = (int)(height)     #cast to int
 
 #process
 #what needs to be done to get volume from these numbers
 
 volume = math.pi*radius*radius*height
+volume = round(volume,2)
 
 #output
 #after calculating what number it output
 
-print("The volume is: ",volume)
+print("\n\tHi "+name+"!")
+print("\n\tGive a cylinder with:")
+print("\n\tRadius = "+str(radius))
+print("\n\tHeight = "+str(height))
+print("\n\tThe volume is: "+str(volume))  
