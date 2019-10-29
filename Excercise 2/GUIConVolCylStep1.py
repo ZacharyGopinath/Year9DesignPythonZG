a8f53aff1a2497a3294d081b14ed88bb68730e9d
Zachary Gopinath <zachary.gopinath@desktop-1jsnd43.ucc.on.ca>
2019-10-29 14:48:55 -0400

    Excercise 2 Update

--- Excercise 2/GUIConVolCylStep1.py
+++ Excercise 2/GUIConVolCylStep1.py
@@ -9,15 +9,5 @@ import math
 
 def calcVolumeCylinder(radius, height):
 	print("Calculate Volume Function")
+
 	
-	if radius >= 0 and height >= 0:
-		volume = math.pi * pow(radius,2) * height
-		volume = round(volume, 2)
-		print(volume)
-	else:
-		print("BAD DATA")
-
-
-print("Start Program")
-calcVolumeCylinder(3, 4)
-print("End Program")
