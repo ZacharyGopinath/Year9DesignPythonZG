1bb6cc92107fa03f9eabd525007b9ecb16dd1234
Zachary Gopinath <zachary.gopinath@GCCYJPRUJ1WK.local>
2020-05-12 19:12:15 -0400

    CTS 3

---
+++ Creating the Solution 3/cts3.js
@@ -0,0 +1,4 @@
+function tableEdit(){
+	var newrow = document.getElementById("table-edit");
+	table-edit.innterHTML = "<tr> <td>test</td> <td>test</td> <td>test</td> <td>test</td> <td>test</td> </tr>"
+}
